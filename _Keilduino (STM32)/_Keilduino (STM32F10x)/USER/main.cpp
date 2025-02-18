268b2664668417135b2506fca6ffdae892b6ffd6
xingke.mo <xingke.mo>
2025-02-18 15:57:22 +0800

    add sch

--- _Keilduino (STM32)/_Keilduino (STM32F10x)/USER/main.cpp
+++ _Keilduino (STM32)/_Keilduino (STM32F10x)/USER/main.cpp
@@ -2,10 +2,93 @@
  * @Author: xingke.mo xingke.mo
  * @Date: 2025-02-18 11:43:35
  * @LastEditors: xingke.mo xingke.mo
- * @LastEditTime: 2025-02-18 15:14:30
- * @FilePath: \_Arduino-Keil-STM32\_Keilduino (STM32)\_Keilduino (STM32F10x)\USER\main.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @LastEditTime: 2025-02-18 15:44:50
  */
+
+/*
+第三方库移植示例
+    1.从Arduino的安装库的文件夹中找到要调用的第三方库
+    2.将刚才找到的第三方库放到Keil工程模板的Libraries文件夹内
+    3.启动Keil工程
+    4.将第三方库的源代码文件添加到Keil的工程组
+    5.找到第三方库文件夹
+    6.添加路径
+    7.找到第三方库自带的示例程序(一般位于第三方库目录下的examples内)
+    8.复制示例程序
+    9.将示例程序复制到Keil工程模板的main.cpp里，替换 setup() 和 loop() 函数
+    10.修改示例程序里的引脚定义为STM32的引脚
+    11.点击编译，如果编译无错误表示移植成功
+
+void setup()
+{
+	pinMode(PA0,OUTPUT);			//使用Arduino函数将配置PA0为输出模式
+}
+
+void loop()
+{
+	GPIOA -> BSRR = GPIO_Pin_0;		//使用寄存器将PA0电平拉高
+	delay(1000);				//延时一秒
+	GPIO_ResetBits(GPIOA, GPIO_Pin_0);	//使用标准外设库的函数将PA0电平拉低
+	delay(1000);				//延时一秒
+}
+
+支持Arduino函数有:
+基本:
+时间:
+	delay(Time_ms);
+	delayMicroseconds(Time_us);
+	millis();
+	micros();
+输入输出:
+	pinMode(Pin,Mode);
+	attachInterrrupt(Pin,Callback_function,Trigger_Mode);	
+	digitalWrite(Pin,State);
+	digitalRead(Pin);
+	analogRead(Pin);
+	analogWrite(Pin,DutyCycle);
+	shiftOut(dataPin,clockPin,bitOrder,value);
+	shiftIn(dataPin,clockPin,bitOrder);
+	tone(Pin,Frequency,Time_ms);
+	tone(Pin,Frequency);
+Print
+
+	Serial.print("Hello STM32");
+	Serial.printf("Hello STM%d",32);
+	Serial << "Hello STM32" << "\r\n";
+String
+
+	String s1 = "Hello";
+	String s2 = " STM32";
+	Serial.print(s1 + s2);
+Stream
+
+	Serial.begin(115200);
+	Serial.setTimeout(20);
+	if(Serial.available())
+	{
+		String s = Serial.readString();
+	}
+外设:
+Serial
+
+ 	Serial.begin(115200);
+	while(Serial.available())
+	{
+		Serial.print(Serial.read());
+	}
+Wire
+
+	Wire.begin();
+	Wire.beginTransmission(0xFF);
+	Wire.write(0x01);
+	Wire.endTransmission();
+SPI
+
+	SPI.begin();
+	SPI.transfer(0xFF);
+*/
+
+
 #include "Arduino.h"
 #if 1
 #define LED_Pin PC13
